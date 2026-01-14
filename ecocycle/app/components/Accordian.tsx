feb1cb4782b1176d406b8a3812872ac6885c107e
Etornam Kordzo Ahiataku <123446443+etor01@users.noreply.github.com>
2026-01-14 15:04:49 +0000

    Created Accordian componet

---
+++ ecocycle/app/components/Accordian.tsx
@@ -0,0 +1,9 @@
+
+
+export default function Accordian() {
+    return (
+        <div>
+            This is the Education section on the type on waste and how to manage them.
+        </div>
+    );
+}
